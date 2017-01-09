d3ddb308962d8a8d10e975ef3b27ab56cc1064a5
Aditi994 <helloaditidubey94@gmail.com>
2017-01-09 16:39:29 +0530

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,5 +1,5 @@
 function fun(){
-   document.getElementById("content").innerHTML="<div class='panel-footer bg-2 text-center' style='LINE-HEIGHT:18px;'>"
+   document.getElementById("content").innerHTML="<div class='panel-footer bg-2 text-center' style='LINE-HEIGHT:10px;'>"
       +"<h3>Where to find us?</h3><br>"
       +"<p>Contact us :</p>"
       +"<br>&#9742;:99XXXXXXXX<br>"
