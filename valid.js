d65fb8dbef4c5e8e9df3eda7297b519b17a55960
Aditi994 <helloaditidubey94@gmail.com>
2017-01-10 15:57:37 +0530

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,7 +1,7 @@
 function fun(){
    document.getElementById("content").innerHTML="<div class='panel-footer bg-2 text-center' style='LINE-HEIGHT:2px;'>"
       +"<h3>Where to find us?</h3><br>"
-      +"<p>Contact us :</p>"
+      +"<p>Contact us :</p><br>"
       +"<br>&#9742;:99XXXXXXXX<br>"
-      +"&#x2709;:yatratravel@gmail.com</div>";
+      +"&#x2709;:yatratravel@gmail.com</div><br>";
 }
